0a7afe30a8f1f1951d5a9d673f429cc0483503ae
AtomowyBro <mfarciarz23@gmail.com>
2017-03-20 18:02:33 +0100

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@
 #include <cstdlib>
 #include <time.h>
 
-const int M = 6;
-const int N = 5;
+const int M = 10;
+const int N = 10;
 
 using namespace std;
 
@@ -48,6 +48,8 @@ public:
 	void display();
 	void debug_display();
 	void reveal(int x, int y);
+	bool iscovered(int x, int y);
+	void change_flague(int x, int y);
 };
 
 Plansza::Plansza()
@@ -55,16 +57,102 @@ Plansza::Plansza()
 	;
 }
 
+void saper(Plansza pole);
 
 int main()
 {
-	srand((0)); //umozliwia losowe rozmieszczenie za karzdym uruchomieniem programu
+	srand(time(0)); //umozliwia losowe rozmieszczenie za karzdym uruchomieniem programu
 	Plansza pole;
 	pole.deploy_mines(1, 1);
-	pole.reveal(5, 0); //popsute
 	pole.display();
+	saper(pole);
 	system("pause");
+
 	return 0;
+}
+
+void saper(Plansza pole)
+{
+	int x, y;
+	int ilosc_min = 0;
+	for (int i = 0;i < M;i++) //zliczam miny na planszy
+	{
+		for (int j = 0;j < N;j++)
+		{
+			if (pole.hasMine(i, j))
+				ilosc_min++;
+		}
+	}
+	bool koniec_gry = false;
+	bool sukces = false;
+	while (!koniec_gry)
+	{
+		system("cls");
+		pole.display();
+		int ilosc_zakrytych = 0;
+		cout << "1. odkryj pole x,y" << endl;
+		cout << "2. zmien flage x,y" << endl;
+		int akcja;
+		cin >> akcja;
+		switch (akcja) // menu z 2 opcjami
+		{
+		case 1:
+		{
+			cout << "Podaj pole 'x' do odkrycia: ";
+			cin >> x;
+			cout << "Podaj pole 'y' do odkrycia: ";
+			cin >> y;
+			pole.reveal(x, y);
+			if (pole.hasMine(x, y)) //jezeli odkryjemy pole z mina nastepuje koniec gry
+			{
+				koniec_gry = true;
+			}
+		}
+		break;
+		case 2:
+		{
+			cout << "Podaj pole 'x' do zmiany flagi: ";
+			cin >> x;
+			cout << "Podaj pole 'y' do zmiany flagi: ";
+			cin >> y;
+		}
+		pole.change_flague(x, y);
+		break;
+		default:
+			cout << "Brak takiej opcji!" << endl;
+			system("pause");
+			break;
+		}
+		for (int i = 0;i < M;i++) // zliczam zawsze od nowa ilosc zakrytych
+		{
+			for (int j = 0;j < N;j++)
+			{
+				if (pole.iscovered(i, j))
+					ilosc_zakrytych++;
+			}
+		}
+		if (ilosc_min == ilosc_zakrytych) // jezeli jest tyle samo zakrytych co min koncze gre i wygrywam
+		{
+			koniec_gry = true;
+			sukces = true;
+		}
+	}
+	system("cls");
+	if (sukces)
+		cout << "Wygrana"<<endl;
+	else
+		pole.display();
+
+}
+
+void Plansza::change_flague(int x, int y)
+{
+	tab[x][y].change_flague();
+}
+
+bool Plansza::iscovered(int x, int y)
+{
+	return tab[x][y].iscovered();
 }
 
 void field::place_mine() //umieszcza mine na polu
@@ -89,13 +177,14 @@ void field::change_flague() // zmienia flage na polu
 void Plansza::reveal(int x, int y)
 {
 	tab[x][y].Uncover();
-	if (!count_mines(x,y))
+	//	if(x<0||x>=M||)
+	if (!count_mines(x, y))
 	{
 		int i = 0; //zmienne ktore beda pomocne przy ilosci pol w zaleznosci od tego ktore pole odkrywamy (6 przy krawedzi, 4 w rogu, 9 w srodku)
 		int j = 0;
 		int i_max = 3;
 		int j_max = 3;
-		if (x == 0) //dzieki tym zmiennym moge wszystkie mozliwosci wykonac w jednej petli 
+		if (x == 0) //dzieki tym zmiennym moge wszystkie mozliwosci wykonac w jednej petli
 		{
 			i = 1;
 		}
@@ -115,7 +204,7 @@ void Plansza::reveal(int x, int y)
 		{
 			for (int j_pom = j;j_pom < j_max;j_pom++) //dzieki i_pom i j_pom jestem w stanie korygowac i ustawiac wczesniej zadana wartosc
 			{
-				//tab[x-1+i_pom][y-1+j_pom].Uncover(); //stara opcja 
+				//tab[x-1+i_pom][y-1+j_pom].Uncover(); //stara opcja
 				if (tab[x - 1 + i_pom][y - 1 + j_pom].iscovered()) //nowa lepsza jezeli pobliskie pole jest zakryte to rekurenkcyjnie wywoluja ta funkcje na tamtym polu
 					reveal(x - 1 + i_pom, y - 1 + j_pom);
 			}
@@ -130,7 +219,7 @@ int Plansza::count_mines(int x, int y)
 	int j = 0;
 	int i_max = 3;
 	int j_max = 3;
-	if (x == 0) //dzieki tym zmiennym moge wszystkie mozliwosci wykonac w jednej petli 
+	if (x == 0) //dzieki tym zmiennym moge wszystkie mozliwosci wykonac w jednej petli
 	{
 		i = 1;
 	}
